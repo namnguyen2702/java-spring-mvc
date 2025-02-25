1402719e97edba69c1ed38a1fd3dd4e75c93d040
Nguyen Van Nam <namnguyen.27022003@gmail.com>
2025-02-25 17:23:27 +0700

    send data to form

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,38 +1,40 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
-
 
 @Controller
 public class UserController {
 
     private UserService userService;
-    
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    @GetMapping("/")
-    public String getHomePage(){
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.HandleHello();
+        model.addAttribute("eric", test);
 
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println(hoidanit);
+        return "hello";
+    }
 }
-
-// @RestController
-// public class UserController {
-// //DI
-//     private UserService userService;
-    
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-//     @GetMapping("")
-//     public String getHomePage(){
-
-//         return this.userService.HandleHello();
-//     }
-// }
